b9121bf604dd0316586cd6177469dde32719aca5
Maeva <maeva.desab@gmail.com>
2024-11-25 09:29:33 +0100

    add Gson

--- app/src/main/java/fr/enssat/singwithme/MdNs/MainActivity.kt
+++ app/src/main/java/fr/enssat/singwithme/MdNs/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello o!",
+        text = "Hello !",
         modifier = modifier
     )
 }
