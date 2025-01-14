20030788588e8c3dcb3a6bb2a04cdc85fa8f53fa
Maeva <maeva.desab@gmail.com>
2025-01-14 17:33:07 +0100

    fix problem with internet and download

--- app/src/main/java/fr/enssat/singwithme/MdNs/MainActivity.kt
+++ app/src/main/java/fr/enssat/singwithme/MdNs/MainActivity.kt
@@ -1,9 +1,13 @@
 package fr.enssat.singwithme.MdNs
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 import fr.enssat.singwithme.MdNs.Navigation.AppNavigation
 import fr.enssat.singwithme.MdNs.ui.theme.SongWithMeTheme
 
@@ -15,21 +19,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         Thread {
-            // Mise à jour de notre liste de musiques
-            val isUpdated = downloadAndUpdateJson(this, JSON_URL)
-            val jsonData = loadJson(this)
-            val songs = jsonData?.let { parseJson(it) } ?: emptyList()
-
-            runOnUiThread {
-                if (isUpdated) {
-                    Toast.makeText(this, "Playlist mise à jour", Toast.LENGTH_SHORT).show()
+            // si fichier de sauvegarde present
+            var jsonData = loadJson(this)
+            var songs = jsonData?.let { parseJson(it) } ?: emptyList()
+            if (songs.isNotEmpty()) {
+                runOnUiThread {
+                    setContent {
+                        SongWithMeTheme {
+                            AppNavigation(songs = songs)
+                        }
+                    }
                 }
-                setContent {
-                    SongWithMeTheme {
-                        AppNavigation(songs = songs)
+            }else if(!checkInternet(this)){
+                // check internet
+                runOnUiThread {
+                    setContent {
+                        SongWithMeTheme {
+                            ErrorConnexion()
+                        }
+                    }
+                }
+            }else{
+                // Mise à jour de notre liste de musiques
+                val isUpdated = downloadAndUpdateJson(this, JSON_URL)
+                jsonData = loadJson(this)
+                songs = jsonData?.let { parseJson(it) } ?: emptyList()
+                runOnUiThread {
+                    if (isUpdated) {
+                        Toast.makeText(this, "Playlist mise à jour", Toast.LENGTH_SHORT).show()
+                    }
+                    setContent {
+                        SongWithMeTheme {
+                            AppNavigation(songs = songs)
+                        }
                     }
                 }
             }
+
         }.start()
+    }
+
+    private fun checkInternet(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
 }
