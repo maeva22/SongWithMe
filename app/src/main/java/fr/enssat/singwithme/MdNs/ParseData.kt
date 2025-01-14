fa989ac2f513a95cc88a23ccda071fd0a8e0b4d9
Maeva <maeva.desab@gmail.com>
2025-01-14 18:17:44 +0100

    Fix : parole

--- app/src/main/java/fr/enssat/singwithme/MdNs/ParseData.kt
+++ app/src/main/java/fr/enssat/singwithme/MdNs/ParseData.kt
@@ -1,7 +1,5 @@
 package fr.enssat.singwithme.MdNs
 
-import androidx.annotation.Nullable
-
 data class ParoleParse(
     val txt: String,
     val timerStart: Int,
@@ -13,10 +11,10 @@ fun initParoleParle(txt: String, timerStart: Int, timerEnd: Int, end : Boolean) 
     return ParoleParse(txt = txt, timerStart = timerStart, timerEnd =  timerEnd, end = end)
 }
 
-fun tabToString(textsong: Karaoke) : String {
+fun tabToString(textsong: String) : String {
     var tps = ""
-    for (value in textsong.lyrics) {
-        tps += value.text + "\n"
+    for (value in textsong) {
+        tps += value + "\n"
     }
     return tps
 }
@@ -24,8 +22,8 @@ fun timeCodeToMs(timeCode : String) : Int{
     return (timeCode.split(':')[0]).toInt()*60000 + (timeCode.split(':')[1]).toInt()*1000
 }
 
-fun transformToData(textsong:String){
-    val regexRule  ="""(?=(?>\{ *([^\n {}]+) *}([^\n{}]+\n?)(?>\{ *([^\n {}]+) *}(\n)?)))"""
+fun transformToData(textsong:String): MutableList<ParoleParse> {
+    val regexRule  ="""(?=(?>\{ *([^\n {}]+) *\}([^\n{}]+\n?)(?>\{ *([^\n {}]+) *\}(\n)?)))"""
     val regex = Regex(regexRule,RegexOption.MULTILINE)
 
     val result: Sequence<MatchResult>  =  regex.findAll(textsong)
@@ -36,7 +34,9 @@ fun transformToData(textsong:String){
     for(pack in result){
         val check =  pack.groups[2]!!.value.indexOf("\n")!=-1 || pack.groups[4] !=null
         tab.add(
-            ParoleParse(txt = pack.groups[2]!!.value.replace('\n',' ') , timerStart =timeCodeToMs(pack.groups[1]!!.value) , timerEnd =timeCodeToMs(pack.groups[3]!!.value), end =check )
+            ParoleParse(txt = pack.groups[2]!!.value.replace('\n',' ') ,
+                timerStart =timeCodeToMs(pack.groups[1]!!.value) ,
+                timerEnd =timeCodeToMs(pack.groups[3]!!.value), end =check )
         )
     }
 
@@ -44,6 +44,7 @@ fun transformToData(textsong:String){
         print( line.timerStart.toString() + " / " + line.timerEnd.toString() + " : " + line .txt )
         if( line.end) print('\n')
     }
+    return tab
 
 }
 
