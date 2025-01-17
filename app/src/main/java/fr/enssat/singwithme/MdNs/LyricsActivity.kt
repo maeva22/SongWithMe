95663f0946110695496d55191b2ee5e36b897269
Sipores <nathansans2610@gmail.com>
2025-01-17 18:53:32 +0100

    Base

--- app/src/main/java/fr/enssat/singwithme/MdNs/LyricsActivity.kt
+++ app/src/main/java/fr/enssat/singwithme/MdNs/LyricsActivity.kt
@@ -11,9 +11,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,10 +26,9 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.media3.exoplayer.ExoPlayer
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
-import java.io.File
+import kotlin.reflect.KProperty
 
 
 const val BASE_URL = "https://gcpa-enssat-24-25.s3.eu-west-3.amazonaws.com/"
@@ -42,39 +41,17 @@ fun LyricsScreen(songPath: String?, navController: NavController) {
         val mdContent = dowloadMd(context, songPath.toString())
         val karaoke = mdContent?.let { parseMd(it) }
         val launchMusic = remember { mutableStateOf(false) }
-        var mP3File by remember { mutableStateOf<File?>(null) }
-        var exoPlayer by remember { mutableStateOf<ExoPlayer?>(null) }
-
-        // Télécharger le fichier MP3 si nécessaire
-        LaunchedEffect(songPath) {
-            mP3File = songPath?.let { downloadSong(context, it) }
-        }
-
-        DisposableEffect(Unit) {
-            onDispose {
-                exoPlayer?.release() // Libérer le lecteur lorsqu'il n'est plus nécessaire
-            }
-        }
         // Affichage de l'écran
         Column(modifier = Modifier.padding(16.dp)) {
             // Bouton pour revenir à la liste des chansons
             Button(
                 modifier = Modifier.padding(bottom = 16.dp),
-                onClick = { navController.navigateUp()
-                    exoPlayer?.stop()}
+                onClick = { navController.navigateUp() }
             ) {
                 Text(text = "Retour")
             }
             ElevatedButton(
-                onClick = {
-                    launchMusic.value = !launchMusic.value
-                    if (launchMusic.value) {
-                        // Initialiser ExoPlayer
-                        exoPlayer = mP3File?.let { playSong(context, it) }
-                    } else {
-                        exoPlayer?.stop()
-                    }
-                }
+                onClick = { launchMusic.value = !launchMusic.value }
             ) {
                 Text(if (launchMusic.value) "Stop" else "Play")
             }
@@ -91,6 +68,7 @@ fun LyricsScreen(songPath: String?, navController: NavController) {
                     // GET the data to good format
                     val textSong = parseMd(karaoke)
                     val parole = transformToData(textSong)
+
                     KarokeBox(parole)
                 }
             }
@@ -100,11 +78,9 @@ fun LyricsScreen(songPath: String?, navController: NavController) {
             // Bouton pour revenir à la liste des chansons
             Button(
                 modifier = Modifier.padding(bottom = 16.dp),
-                onClick = { navController.navigateUp()
-                }
+                onClick = { navController.navigateUp() }
             ) {
-                Text(text = "Retour"
-                )
+                Text(text = "Retour")
             }
             Text(
                 text = "Les paroles sont actuellement indisponible !",
@@ -122,7 +98,7 @@ fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
 fun KaraokeSimpleText(text: String, progress: Float) {
     var width by remember { mutableIntStateOf(0) }
     var height by remember { mutableIntStateOf(0) }
-
+    //Log.d("LectureString", " Width : $width / Height: $height Progress : $progress")
 
     Box(Modifier.padding(Dp(50f))) {
         Text(
@@ -131,6 +107,7 @@ fun KaraokeSimpleText(text: String, progress: Float) {
             modifier = Modifier.onSizeChanged {
                 width = it.width
                 height = it.height
+                Log.d("Verif Change" , " Width : $width / Height: $height")
             },
             textAlign = TextAlign.Left
         )
@@ -142,47 +119,90 @@ fun KaraokeSimpleText(text: String, progress: Float) {
                 .padding(
                     start = width.pxToDp() * progress,
                     end = width.pxToDp() - (width.pxToDp() * progress)
+
                 )
         ) {}
 
         Text(
             text = text,
-            color = Color.White,
+            color = Color.Blue,
             modifier = Modifier.width(width.pxToDp() * progress),
             textAlign = TextAlign.Left,
             softWrap = false
         )
 
     }
-    Log.d("LectureString", "Width : $width , Progress : $progress")
+
 
 }
 
 @Composable
-fun KaraokeSimpleTextAnimate(duration: Int, text: String,key: Int) {
+fun KaraokeSimpleTextAnimate(List:List<ParoleParse>) {
+
     val karaokeAnimation = remember { Animatable(0f) }
 
-    LaunchedEffect(key) {
-        karaokeAnimation.snapTo(0f)
+    var index by remember { mutableIntStateOf(0) }
+    var TargetValue by remember { mutableFloatStateOf(0f) }
+    var text= ""
 
-        karaokeAnimation.animateTo(1f, tween(duration, easing = LinearEasing))
+    for (line in List){
+        text += line.txt;
     }
-    Log.d("LectureString", "Animation : ${karaokeAnimation.value}")
+
+    LaunchedEffect(UInt,index) { // Valeur différente a chaque appel pour "reset le truic ! "
+
+        karaokeAnimation.snapTo(0f)
+        var letmot = 0
+        for(i in 0..<List.size){
+            letmot+= List[i].txt.length
+        }
+
+        TargetValue = 0f
+        TargetValue = (List[index].txt.length / letmot).toFloat()
+
+        if(TargetValue>1){
+            Log.d("ERROR", "Target Value :$TargetValue / WordLen :${List[index].txt.length} / Word total :$letmot")
+        }
+
+        val duration = (List[index].timerEnd- List[index].timerStart )
+        karaokeAnimation.animateTo(TargetValue, tween(duration, easing = LinearEasing))
+
+        if (index < List.size-1) {
+            delay(duration.toLong())  // Attendre la durée de l'animation
+            index += 1  // Incrémenter l'indice pour le texte suivant
+        }
+    }
+
     KaraokeSimpleText(text, karaokeAnimation.value)
 }
 @Composable
 fun KarokeBox(parole: List<ParoleParse>){
 
     var indexString by remember { mutableStateOf(1) }
+    var nbletter by remember { mutableStateOf(1) }
     val line = parole[indexString]
-    val text = line.txt
-    val duration =  line.timerEnd - line.timerStart
+
+    val tps by remember { mutableStateOf(mutableListOf<ParoleParse>())   }
 
     LaunchedEffect(indexString) {
-        if (indexString < parole.size) {
-            delay(duration.toLong())  // Attendre la durée de l'animation
-            indexString += 1  // Incrémenter l'indice pour le texte suivant
+        nbletter=0
+        tps.clear()
+        var decal = indexString
+        while(!parole[decal].end) {
+            nbletter+=parole[decal].txt.length
+            tps.add(parole[decal])
+            decal+=1
+        }
+        tps.add(parole[decal])
+
+        if (decal+1 < parole.size) {
+            delay((tps.last().timerEnd - tps.first().timerStart).toLong())  // Attendre la durée de l'animation
+            indexString = decal+1  // Incrémenter l'indice pour le texte suivant
         }
     }
-    KaraokeSimpleTextAnimate(duration,text,indexString)
+
+    KaraokeSimpleTextAnimate(tps)
+
+
 }
+
